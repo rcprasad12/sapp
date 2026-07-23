af30e25fe473d7c5992c64b18451a01c1b18ddba
RC Prasad <rcprasad.2000@gmail.com>
2026-07-23 13:11:38 +0530

    deploy

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,18 +1,14 @@
 import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const connectionString = process.env.DATABASE_URL!;
-
-const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
 
